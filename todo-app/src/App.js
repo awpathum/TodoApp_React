d73767c3d30e77e7d4191ba28ef1dde64cdc85a9
pathum <awpathum@gmail.com>
2020-03-05 23:28:15 +0530

    Adding button and counter

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import FirstComponent from './components/learning-examples/FirstComponent';
 import SecondComponent from './components/learning-examples/SecondComponent';
 import ThirdComponent from './components/learning-examples/ThirdComponent';
-
+import Counter from './components/counter/Counter';
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,11 +14,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Counter></Counter>
+      </div>
+    );
+  }
+}
+
+
+class LearningComponent extends Component {
+  render() {
+    return (
+      <div className="LearningComponent">
         My Hello World!
         <FirstComponent></FirstComponent>
         <SecondComponent></SecondComponent>
         <ThirdComponent></ThirdComponent>
-      </div>
+      </div> 
     );
   }
 }
