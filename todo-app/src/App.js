a8b853c1448e6ec987cda1192f0bc06a144f831c
pathum <awpathum@gmail.com>
2020-03-05 23:11:54 +0530

    components created

---
+++ todo-app/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import FirstComponent from './components/learning-examples/FirstComponent';
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+
+import logo from './logo.svg';
+import './App.css';
+
+//import FirstComponent from './components/learning-examples/FirstComponent'
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        My Hello World!
+        <FirstComponent></FirstComponent>
+        <SecondComponent></SecondComponent>
+        <ThirdComponent></ThirdComponent>
+      </div>
+    );
+  }
+}
+
+export default App;
