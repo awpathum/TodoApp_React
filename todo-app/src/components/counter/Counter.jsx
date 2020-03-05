978e85e5db8c6f690d2566ec30d0fe778a709cf3
pathum <awpathum@gmail.com>
2020-03-06 01:46:45 +0530

    couter created with 3 buttons

--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -3,11 +3,38 @@ import PropTypes from 'prop-types'
 import './Counter.css'
 
 
+
 class Counter extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            counter: 0,
+        }
+        this.increment = this.increment.bind(this);
+    }
+    render() {
+        return (
+            <div className="counter">
+                <CounterButton by={1} incrementMethod={this.increment}></CounterButton>
+                <CounterButton by={5} incrementMethod={this.increment}></CounterButton>
+                <CounterButton by={10} incrementMethod={this.increment}></CounterButton>
+                <span className="count" >{this.state.counter}</span>
+            </div>
+        )
+    }
+    increment(by) {
+        //sconsole.log(`increment from parent ${by}`);
+        this.setState({
+            counter: this.state.counter + by
+        })
+    }
+}
+
+class CounterButton extends Component {
 
     //Define the initial state in the constructor
     //state => counter 0
-
     constructor() {
         super();
         this.state = {
@@ -22,21 +49,19 @@ class Counter extends Component {
         return (
             <div className="counter">
                 <button onClick={this.increment}>+{this.props.by}</button>
-                <span className="count" >{this.state.counter}</span>
+                {/* <span className="count" >{this.state.counter}</span> */}
 
             </div>
         );
     }
 
-    //Update the state = counter++
     increment() {
-        //console.log('increment');
+
         this.setState({
             counter: this.state.counter + this.props.by
-            // secondCounter: this.state.secondCounter + 1
+
         })
-
-
+        this.props.incrementMethod(this.props.by);
 
     }
 
@@ -50,6 +75,5 @@ Counter.defaultProps = {
 Counter.prototypes = {
     by: PropTypes.number
 }
-
 
 export default Counter;
