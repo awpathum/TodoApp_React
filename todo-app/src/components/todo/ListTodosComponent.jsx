022a269e28f566c68ae4e8153eb29c2fdd143685
pathum <awpathum@gmail.com>
2020-03-07 21:41:03 +0530

    refactored components to seperate files

---
+++ todo-app/src/components/todo/ListTodosComponent.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class ListTodosComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            todos: [{ id: 1, description: 'Learn React', targetDate: new Date(), done: false }, { id: 2, description: 'Learn Well', targetDate: new Date(), done: false }, { id: 3, description: 'Be happy', targetDate: new Date(), done: false }]
+        }
+    }
+    render() {
+        return <div>
+            <h1>
+                List Todos
+            </h1>
+            <div className="container">
+                <table className="table">
+                    <thead>
+                        <tr>
+
+                            <th>Description</th>
+                            <th>Target Date</th>
+                            <th>Is completed</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.todos.map(
+                                todo => <tr key={todo.id}>
+                                    <td>{todo.description}</td>
+                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.targetDate.toString()}</td>
+                                </tr>
+                            )
+
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    }
+}
+
+export default ListTodosComponent
---
+++ todo-app/src/components/todo/LoginComponent.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import AuthenticationService from './AuthenticationService.js'
+import AuthenticatedRoute from './AuthenticatedRoute.jsx'
+
+class LoginComponent extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: '',
+            password: '',
+            hasLoginFailed: false,
+            showSucesssMessage: false,
+            //isUserLogedIn:false
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
+    }
+
+    handleChange(event) {
+        console.log(event.target.value);
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    loginClicked(event) {
+        if (this.state.username === "mamba" && this.state.password === "mamba123") {
+
+            //AuthenticationService.registerSuccessfulLogin(this.state.username, this.state.password);
+            AuthenticationService.registerSuccessfulLogin(this.state.username);
+            this.props.history.push(`/welcome/${this.state.username}`)
+        }
+        else {
+            console.log('Failed')
+            this.setState({
+                showSucesssMessage: false,
+                hasLoginFailed: true
+            })
+        }
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Login</h1>
+                <div className="container">
+                    {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
+                    {this.state.showSucesssMessage && <div>Login Successful</div>}
+                    Username : <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+                    Password : <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                    <button className="btn btn-sucess" onClick={this.loginClicked}>Login</button>
+                </div>
+            </div>
+        )
+    }
+
+}
+export default LoginComponent
