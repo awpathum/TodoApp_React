5b4924347ddff0b2cafbdfa64d8cb487ea711ea8
Pathum Wijethunge <awpathum@gmail.com>
2020-03-08 17:48:34 +0530

    todo coamponent form validated

--- todo-app/src/components/todo/TodoComponent.jsx
+++ todo-app/src/components/todo/TodoComponent.jsx
@@ -1,9 +1,78 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 class TodoComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: 1,
+            description: 'Learn More',
+            targetDate: moment(new Date()).format('YYYY-MM-DD')
+        }
+        this.onSubmit = this.onSubmit.bind(this)
+        this.validate = this.validate.bind(this)
+    }
+
+    validate(values) {
+
+        let errors = {}
+        if (!values.description) {
+            errors.description = "Enter a Description"
+        } else if (values.description.length < 5) {
+            errors.description = "Enter atleast 5 Characters in Description"
+        }
+
+        if (!moment(values.targetDate).isValid()) {
+            errors.targetDate = "Enter a Valid Target Date"
+        }
+        console.log(values)
+        return errors
+    }
+
+    onSubmit(values) {
+        console.log(values)
+    }
+
     render() {
-        return (<div>
-            <p>Todo Component</p>
-        </div>)
+        let { description, targetDate } = this.state
+        return (
+            <div>
+                <h1>Todo</h1>
+                <div className="container">
+                    <Formik
+                        initialValues={{
+                            description,
+                            targetDate
+                        }}
+                        onSubmit={this.onSubmit}
+                        validateOnChange={false}
+                        validateOnBlur={false}
+                        validate={this.validate}
+                    >
+                        {
+                            (props) => (
+                                <Form>
+                                    <ErrorMessage name="description" component="div" className="alert alert-warning"></ErrorMessage>
+                                    <ErrorMessage name="targetDate" component="div" className="alert alert-warning"></ErrorMessage>
+                                    <fieldset className="form-group">
+                                        <label>Description</label>
+                                        <Field className="form-control" type="text" name="description"></Field>
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Target Date</label>
+                                        <Field className="form-control" type="date" name="targetDate"></Field>
+                                    </fieldset>
+                                    <button className="btn btn-success" type="submit">Save</button>
+                                </Form>
+
+                            )}
+
+                    </Formik>
+
+                </div>
+            </div>
+        )
     }
 
 }
